15b9acfed55bfca05aba4675dc16b2b5004fff2f
Stacy Kiriiri <Wkiriiri@gmail.com>
2020-04-06 00:31:11 +0300

    adds a hover effect on the portfolios

--- js/scripts.js
+++ js/scripts.js
@@ -10,6 +10,55 @@ $(document).ready(function() {
     $('.clickable2').click(function () {
         $('.product-description').slideToggle();
         $('.product-icon').slideToggle();
-    });  
-    
+    });
 });
+$(document).ready(function(){
+    $("#portfolio1").mouseover(function(){
+      $("#overlay1").show();
+    }).mouseout(function(){
+      $("#overlay1").hide();
+    });
+  });
+  $(document).ready(function(){
+    $("#portfolio2").mouseover(function(){
+      $("#overlay2").show();
+    }).mouseout(function(){
+      $("#overlay2").hide();
+    });
+  });
+  $(document).ready(function(){
+    $("#portfolio3").mouseover(function(){
+      $("#overlay3").show();
+    }).mouseout(function(){
+      $("#overlay3").hide();
+    });
+  });
+  $(document).ready(function(){
+    $("#portfolio4").mouseover(function(){
+      $("#overlay4").show();
+    }).mouseout(function(){
+      $("#overlay4").hide();
+    });
+  });
+  $(document).ready(function(){
+    $("#portfolio5").mouseover(function(){
+      $("#overlay5").show();
+    }).mouseout(function(){
+      $("#overlay5").hide();
+    });
+    $("#portfolio6").mouseover(function(){
+      $("#overlay6").show();
+    }).mouseout(function(){
+      $("#overlay6").hide();
+    });
+    $("#portfolio7").mouseover(function(){
+      $("#overlay7").show();
+    }).mouseout(function(){
+      $("#overlay7").hide();
+    });
+    $("#portfolio8").mouseover(function(){
+      $("#overlay8").show();
+    }).mouseout(function(){
+      $("#overlay8").hide();
+    });
+  });
