7c1fd47154806359c9a47a398bce065398138f71
Stacy Kiriiri <Wkiriiri@gmail.com>
2020-04-05 14:59:03 +0300

    removes the javascript code for submitting the form

--- js/scripts.js
+++ js/scripts.js
@@ -13,25 +13,3 @@ $(document).ready(function() {
     });  
     
 });
-document.addEventListener('DOMContentLoaded',function() {
-    document.getElementById("submit").addEventListener("click", function(event) {
-        event.preventDefault()
-    })
-});
-function submitData() {
-    var name=document.getElementById("name").value;
-    var email=document.getElementById("e-mail").value;
-    var message=document.getElementById("message").value;
-    if(name === "") {
-        document.getElementById("name").innerText="Enter your name please"
-        document.getElementById("error").style.color="red"
-    }else if(email ==="") {
-        document.getElementById("email").innerText="Enter your E-mail please"
-        document.getElementById("errro1").style.color="red"
-    }else if(message==="") {
-        document.getElementById("message").innerText="Enter your message please"
-        document.getElementById("error2").style.color="red"
-    }else{
-        document.getElementById("contact-us").innerHTML="Thanks for the feedback"
-    }
-}
